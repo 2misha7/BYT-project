b899cd5e4017077e886dba399c0d328ebac8ba41
julia <juliapostcard@gmail.com>
2024-10-21 19:56:43 +0200

    added  ReviewRating.cs , filled Post.cs

--- BookingApp/BookingApp/Models/Post.cs
+++ BookingApp/BookingApp/Models/Post.cs
@@ -1,6 +1,11 @@
 ﻿namespace BookingApp.Models;
 
-public class Post
-{
-    
+public class Post(string imageLink, int portfolioPageId, string text)
+{   public string ImageLink { get; set; } = imageLink;
+    public int PortfolioPage { get; set; } = portfolioPageId;
+    public string Text { get; set; } = text;
+    public int Likes { get; set; } = 0;
+    public int Dislikes { get; set; } = 0;
+    public ICollection<string> Comments { get; set; } = new HashSet<string>();
+
 }
