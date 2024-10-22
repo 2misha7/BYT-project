36a63cef9ee4fbbd0881afd852361007f964d908
Mikhalina Kuliashova <mikhalina10@gmail.com>
2024-10-22 10:29:48 +0200

    coupon class modified

--- BookingApp/BookingApp/Models/Coupon.cs
+++ BookingApp/BookingApp/Models/Coupon.cs
@@ -1,8 +1,8 @@
 ﻿namespace BookingApp.Models;
 
-public class Coupon(string code, string description, int discountPercentage, DateTime validFrom, DateTime validTo)
+public class Coupon(string couponCode, string description, int discountPercentage, DateTime validFrom, DateTime validTo)
 {
-    public string Code { get; set; } = code;
+    public string CouponCode { get; set; } = couponCode;
     public string Description { get; set; } = description;
     public int DiscountPercentage { get; set; } = discountPercentage;
     public DateTime ValidFrom { get; set; } = validFrom;
