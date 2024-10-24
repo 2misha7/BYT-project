28a2cbd9553171520e69d28e33afe32d3f0c848d
Mikhalina Kuliashova <mikhalina10@gmail.com>
2024-10-24 13:14:48 +0200

    change2

--- BookingApp/BookingApp/Models/Booking.cs
+++ BookingApp/BookingApp/Models/Booking.cs
@@ -3,7 +3,7 @@
 public class Booking(Guid customerId, ICollection<Guid> serviceBookedId) : IEntity
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    //will be calculated while saving a booking to the system 
+    //will be calculated while saving a booking to the systemqq 
     public decimal? TotalPrice { get; set; }
     public Guid CustomerId { get; set; } = customerId;
     public ICollection<Guid> ServiceBookedId { get; set; } = serviceBookedId;
